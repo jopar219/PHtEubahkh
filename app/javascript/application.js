880df9d205117df3b82195696198e4100412bc83
user <jopar219@gmail.com>
2023-05-23 19:18:26 -0600

    Configures React with Rails

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
-import "./controllers"
+
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+
+document.addEventListener("turbo:load", () => {
+  const root = createRoot(
+    document.body.appendChild(document.createElement("div"))
+  );
+  root.render(React.createElement(App, null, null));
+});
---
+++ app/javascript/components/Index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default () => (
+  <div>
+    <h1>React App</h1>
+    <p>A react app in a Rails repo</p>
+  </div>
+);
